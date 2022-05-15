c7d7001f8c50bc517714e2178f9cb0574cebaffa
Raphael <81021823+raph-exe@users.noreply.github.com>
2022-05-15 09:45:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function shows() {
     document.getElementsByClassName('video')[0].remove();
     document.body.style.backgroundImage = "url('./background.jpg')";
     document.getElementsByTagName('ul')[0].style.display = 'flex';
+    document.getElementsByTagName('ul')[0].style.visibility = 'visible';
 }
