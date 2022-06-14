fba55a4c4debf0a345da72e2ceee335cf44373a9
Raphael <81021823+raph-exe@users.noreply.github.com>
2022-06-14 09:58:28 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function shows() {
-    document.getElementsByTagName('ul')[0].style.display = 'flex';
-    document.getElementsByTagName('ul')[0].style.visibility = 'visible';
-    document.getElementsByTagName('ul')[0].style.zIndex = '2';
-}
